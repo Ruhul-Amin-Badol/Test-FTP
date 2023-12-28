a54af53f28d523449f163ceada772682b03ab4ed
Ruhul-Amin-Badol <ruhul.gub@gmail.com>
2023-12-28 12:58:06 +0600

    up

--- move_upload_file.php
+++ move_upload_file.php
@@ -3,16 +3,16 @@
 // Include the AWS SDK for PHP autoloader
 require 'vendor/autoload.php'; // Replace with the actual path to aws-autoloader.php
 
-// use Aws\S3\S3Client;
-// use Aws\Exception\AwsException;
+use Aws\S3\S3Client;
+use Aws\Exception\AwsException;
 
 //Local upload details
 $localUploadDirectory = "../upload/"; // Replace with the actual path to your local upload directory
 
 // AWS S3 details
-// $AWS_S3_BUCKET = "nbrs3bucket";
-// $AWS_S3_DIRECTORY = "testfolder";
-// $AWS_REGION = "ap-south-1";
+$AWS_S3_BUCKET = "nbrs3bucket";
+$AWS_S3_DIRECTORY = "testfolder";
+$AWS_REGION = "ap-south-1";
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -31,25 +31,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded locally.";
 
                 // Upload the file to AWS S3
-                // $s3 = new S3Client([
-                //     'version' => 'latest',
-                //     'region' => $AWS_REGION,
-                // ]);
+                $s3 = new S3Client([
+                    'version' => 'latest',
+                    'region' => $AWS_REGION,
+                ]);
 
-                // try {
-                //     $result = $s3->putObject([
-                //         'Bucket' => $AWS_S3_BUCKET,
-                //         'Key' => $AWS_S3_DIRECTORY . '/' . basename($_FILES["fileToUpload"]["name"]),
-                //         'SourceFile' => $target_file,
-                //         'ACL' => 'public-read', // Adjust ACL as needed
-                //     ]);
+                try {
+                    $result = $s3->putObject([
+                        'Bucket' => $AWS_S3_BUCKET,
+                        'Key' => $AWS_S3_DIRECTORY . '/' . basename($_FILES["fileToUpload"]["name"]),
+                        'SourceFile' => $target_file,
+                        'ACL' => 'public-read', // Adjust ACL as needed
+                    ]);
 
-                //     // Print the URL of the uploaded file in S3
-                //     echo 'Uploaded file to S3: ' . $result['ObjectURL'] . PHP_EOL;
-                // } catch (AwsException $e) {
-                //     // Handle errors
-                //     echo 'Error uploading file to S3: ' . $e->getMessage() . PHP_EOL;
-                // }
+                    // Print the URL of the uploaded file in S3
+                    echo 'Uploaded file to S3: ' . $result['ObjectURL'] . PHP_EOL;
+                } catch (AwsException $e) {
+                    // Handle errors
+                    echo 'Error uploading file to S3: ' . $e->getMessage() . PHP_EOL;
+                }
 
                 // Optionally, you can remove the local file after uploading it to S3
                 unlink($target_file);
